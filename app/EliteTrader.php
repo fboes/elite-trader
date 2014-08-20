c67de0406adb6c299dffa9353c2e60df33f0bb14
Frank Boës <frank.boes@bluemars.net>
2014-08-20 12:38:18 +0200

    Reduced stale time

--- app/EliteTrader.php
+++ app/EliteTrader.php
@@ -11,8 +11,8 @@ class EliteTrader {
 	const STATUS_VERY_OLD = 3;
 
 	const SECONDS_NEW      = 300;
-	const SECONDS_OLD      = 86400;
-	const SECONDS_VERY_OLD = 604800;
+	const SECONDS_OLD      = 43200;
+	const SECONDS_VERY_OLD = 259200;
 
 	protected $pdo;
 	protected $api;
